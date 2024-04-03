1962e59b168de6b123ce264dfad5cb56323f88e5
Daisylimpangog <119760453+Daisylimpangog@users.noreply.github.com>
2024-04-03 08:02:29 +0800

    12

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Get all elements with class "arrow"
   var arrows = document.querySelectorAll(".arrow");
+  var dropdownContents = document.querySelectorAll(".dropdown-content");
 
-  // Add click event listener to each arrow
-  arrows.forEach(function (arrow) {
-    arrow.addEventListener("click", function () {
-      // Toggle the "active" class for the closest dropdown content
-      var dropdownContent = this.parentElement.nextElementSibling;
-      dropdownContent.classList.toggle("active");
+  arrows.forEach(function (arrow, index) {
+    arrow.addEventListener("click", function (event) {
+      event.stopPropagation(); // Prevent the click event from bubbling up
+      toggleDropdown(dropdownContents[index]);
     });
   });
+
+  document.addEventListener("click", function (event) {
+    dropdownContents.forEach(function (content) {
+      if (!content.contains(event.target)) {
+        content.classList.remove("active");
+      }
+    });
+  });
+
+  function toggleDropdown(content) {
+    // Toggle the "active" class for the dropdown content
+    dropdownContents.forEach(function (item) {
+      if (item !== content) {
+        item.classList.remove("active");
+      }
+    });
+    content.classList.toggle("active");
+  }
 });
